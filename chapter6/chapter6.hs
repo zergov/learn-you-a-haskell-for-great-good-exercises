3ff7632a75e1b78f23a70c4e41f6881f9d5bea61
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-02-04 09:46:10 -0500

    Geometry

--- chapter6/chapter6.hs
+++ chapter6/chapter6.hs
@@ -2,6 +2,10 @@ import Data.List
 import Data.Char
 import qualified Data.Map as Map
 
+import qualified Geometry.Sphere as Sphere
+import qualified Geometry.Cuboid as Cuboid
+import qualified Geometry.Cube as Cube
+
 -- nub takes a list an strips duplicates
 -- nub ['a','b','c','a']
 -- >> ['a','b','c']
